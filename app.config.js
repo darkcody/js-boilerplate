f8811ea175449b92261ef2f96aff528d579f7fc6
Brian Reese <Brian_R_Reese@oa.eop.gov>
2017-09-13 16:31:10 -0400

    Add example env variable config for local development.

--- app.config.js
+++ app.config.js
@@ -1,12 +1,10 @@
 import {
   environment
 } from './buildScripts/envDetect.js';
-// const baseUrl = (environment === 'development') ? 'http://localhost:4201/users' : 'http://beta.json-generator.com/api/json/get/4yJfmmFKm';
-const baseUrl = 'http://headlessnightlyth3vcdx4ci.devcloud.acquia-sites.com';
 
 export default {
   environment: environment,
-  baseUrl: baseUrl,
+  baseUrl: process.env.API_BASE_URL || 'http://headlessnightlyth3vcdx4ci.devcloud.acquia-sites.com',
   apiPrefix: '/jsonapi',
   resourcePaths: {
     user: '/user/user',
